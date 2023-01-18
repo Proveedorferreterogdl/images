3d22729c6e33425c02754b18cfb122ae0190ee91
Proveedorferreterogdl <proveedorferreteropfg@gmail.com>
2023-01-18 12:45:03 -0600

    verdion 1.0.15
    Modificacion de jsonArchivos.py para hacer otro json pero con las carpetas

--- static/Images/jsonArchivos.py
+++ static/Images/jsonArchivos.py
@@ -17,12 +17,19 @@ if os.path.isfile(ubicacion + "/archivos.json"): #Validamos que no exista el jso
 
 contenido = os.listdir(ubicacion + '/productos') #Solicitamos en una lista el nombre de las carpetas dentro de la carpeta productos
 dictionario = dict() #Iniciamos un diccionario
+diccionarioCarpetas = dict()
 n = 0 #Iniciamos un contador
+for j in contenido:
+	diccionarioCarpetas[n+1] = contenido[n]
+	n = n+1
+with open(ubicacion + "/carpetas.json","w") as j: #Se crea y se abre un archivo json llamado archivos.json con la indicacion para escribir
+	json.dump(diccionarioCarpetas, j) #See ingresa el diccionario al json
+n = 0
 for i in contenido: #Se crea un for para hacer un guardado del interior de cada carpeta
 	archivos = os.listdir('C:/Users/Pfg-D/OneDrive/Escritorio/images/static/Images/productos/'+contenido[n]) #Se crea una lista con todo el contenido de las carpetas en cuestion
 	dictionario[contenido[n]] = archivos #Se agrega la lista anterior en la posision de la carpeta principal para su separacion entre marcas
 	n = n+1 #Incrementamos el contador
 	#Y se reinicia el for hasta que ya haya terminado con todas las carpetas de productos
 
-with open(ubicacion + "/archivos.json","w") as j:
-	json.dump(dictionario, j)
+with open(ubicacion + "/archivos.json","w") as j: #Se crea y se abre un archivo json llamado archivos.json con la indicacion para escribir
+	json.dump(dictionario, j) #See ingresa el diccionario al json
